7c318bb4bce2031cdb38d7b21e912464388310ba
publicStaticUser <144150774+publicStaticUser@users.noreply.github.com>
2023-09-05 21:56:14 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+
+class Calculator {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два числа (арабских или римских): ");
+        String expression = scanner.nextLine();
+        System.out.println(parse(expression.replace(" ", "")));
+    }
+
+    public static String parse(String expression) throws Exception {
+        int num1;
+        int num2;
+        String oper;
+        String result;
+        boolean isRoman;
+        String[] operands = expression.split("[+\\-*/]");
+        oper = detectOperation(expression);
+        if (oper == null) throw new Exception("Неподдерживаемая математическая операция");
+        if (operands.length != 2) throw new Exception("Должно быть два операнда");
+        if (Roman.isRoman(operands[0]) && Roman.isRoman(operands[1])) {
+            num1 = Roman.convertToArab(operands[0]);
+            num2 = Roman.convertToArab(operands[1]);
+            isRoman = true;
+        }
+        else if (!Roman.isRoman(operands[0]) && !Roman.isRoman(operands[1])) {
+            num1 = Integer.parseInt(operands[0]);
+            num2 = Integer.parseInt(operands[1]);
+            isRoman = false;
+        }
+        else {
+            throw new Exception("Используются одновременно разные системы счисления");
+        }
+        if (num1 > 10 || num2 > 10) {
+            throw new Exception("Числа должны быть от 1 до 10");
+        }
+        int arab = calc(num1, num2, oper);
+        if (isRoman) {
+            if (arab <= 0) {
+                throw new Exception("Римское число должно быть больше нуля");
+            }
+
+            result = Roman.convertToRoman(arab);
+        } else {
+            result = String.valueOf(arab);
+        }
+        return result;
+    }
+
+    static String detectOperation(String expression) {
+        if (expression.contains("+")) return "+";
+        else if (expression.contains("-")) return "-";
+        else if (expression.contains("*")) return "*";
+        else if (expression.contains("/")) return "/";
+        else return null;
+    }
+
+    static int calc(int a, int b, String oper) {
+
+        if (oper.equals("+")) return a + b;
+        else if (oper.equals("-")) return a - b;
+        else if (oper.equals("*")) return a * b;
+        else return a / b;
+    }
+
+}
+
+class Roman {
+    static String[] romanArr = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI",
+            "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV",
+            "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI",
+            "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII",
+            "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII",
+            "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV",
+            "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV",
+            "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII",
+            "XCVIII", "XCIX", "C"};
+
+
+    public static boolean isRoman(String val) {
+        for (int i = 0; i < romanArr.length; i++) {
+            if (val.equals(romanArr[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int convertToArab(String roman) {
+        for (int i = 0; i < romanArr.length; i++) {
+            if (roman.equals(romanArr[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String convertToRoman(int arab) {
+        return romanArr[arab];
+    }
+
+}
